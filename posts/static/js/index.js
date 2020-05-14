2f438570d40d62eed33e59cb05d34da5d248648e
Chase B. Gale <chase.b.gale@gmail.com>
2020-05-14 13:52:08 -0400

    responsive mobile

--- posts/static/js/index.js
+++ posts/static/js/index.js
@@ -1,43 +1,43 @@
-(function($){
-	
-	$('.entry-content').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
+(function ($) {
 
-      var alt = this.alt;
+    $('.entry-content').each(function (i) {
+        $(this).find('img').each(function () {
+            if ($(this).parent().hasClass('fancybox')) return;
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+            var alt = this.alt;
+
+            $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+        });
+
+        $(this).find('.fancybox').each(function () {
+            $(this).attr('rel', 'article' + i);
+        });
     });
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
+    if ($.fancybox) {
+        $('.fancybox').fancybox();
+    }
+
+    $(".mobile-nav-panel").click(function () {
+        $(".nav").toggleClass("active")
     });
-  });
 
-	if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
+    let now = new Date();
+    $("#experience").text(now.getUTCFullYear() - 2001);
 
-  $(".mobile-nav-panel").click(function() {
-    $(".nav").toggleClass("active")
-  });
-
-  let now = new Date();
-  $("#experience").text(now.getUTCFullYear() - 2001);
-
-  fetch("https://chasegale.com/contributions.php", {
-    headers: {
-        'Content-Type': 'text/html',
-        'Accept': 'text/html',
-    },
-    referrerPolicy: 'no-referrer',
-    cache: 'no-cache'
-  }).then(response => {
-      return response.text();
-  }).then(html => {
+    fetch("https://chasegale.com/contributions.php", {
+        headers: {
+            'Content-Type': 'text/html',
+            'Accept': 'text/html',
+        },
+        referrerPolicy: 'no-referrer',
+        cache: 'no-cache'
+    }).then(response => {
+        return response.text();
+    }).then(html => {
         let template = document.createElement('template');
         template.innerHTML = html;
-        
+
         const contributionsSVG = template.content.querySelector('svg.js-calendar-graph-svg');
         const contributionsDOM = document.getElementById('contributions');
 
@@ -49,13 +49,17 @@
             // CSS3 Selectors have a problem with SVG 'text' elements, js now forcing the spear
             txt.style.animationDelay = `${150 + (50 * idx)}ms`;
         });
-        
+
         // We have better plans than the hard-coded transform, ditch it
         contributionsSVG.firstElementChild.removeAttribute('transform');
+        contributionsSVG.setAttribute("width", "100%");
+
+        // Would love to nest scale()/min()/calc() but CSS and SVG don't play nice
+        contributionsDOM.style.setProperty("--scale", Math.min(0.75, document.body.clientWidth / 750));
 
         contributionsDOM.appendChild(contributionsSVG);
-  }).catch(err => {
-    console.error(err);
-  });
+    }).catch(err => {
+        console.error(err);
+    });
 
 })(jQuery);
